44030e4ff9782a4b850b6ed6be86499675021913
mitHSwas <wdev.mithunbiswas@gmail.com>
2022-10-12 23:51:49 +0600

    finally done

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -7,49 +7,24 @@ import './Statistics.css'
 const Statistics = () => {
     const chartData = useLoaderData();
     const rawData = chartData.data;
+    console.log(rawData)
     const data = [
         {
-            name: 'Page A',
-            uv: 4000,
-            pv: 2400,
-            amt: 2400,
+            name: 'React',
+            qn: 8,
         },
         {
-            name: 'Page B',
-            uv: 3000,
-            pv: 1398,
-            amt: 2210,
+            name: 'Javascript',
+            qn: 9,
         },
         {
-            name: 'Page C',
-            uv: 2000,
-            pv: 9800,
-            amt: 2290,
+            name: 'CSS',
+            qn: 8,
         },
         {
-            name: 'Page D',
-            uv: 2780,
-            pv: 3908,
-            amt: 2000,
-        },
-        {
-            name: 'Page E',
-            uv: 1890,
-            pv: 4800,
-            amt: 2181,
-        },
-        {
-            name: 'Page F',
-            uv: 2390,
-            pv: 3800,
-            amt: 2500,
-        },
-        {
-            name: 'Page G',
-            uv: 3490,
-            pv: 4300,
-            amt: 2100,
-        },
+            name: 'Git',
+            qn: 11,
+        }
     ];
 
     return (
@@ -70,8 +45,8 @@ const Statistics = () => {
                 <YAxis />
                 <Tooltip />
                 <Legend />
-                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                <Line type="monotone" dataKey="qn" stroke="#8884d8" activeDot={{ r: 8 }} />
+
             </LineChart>
         </div>
     );
